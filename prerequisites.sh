4c3f97a08249e7a94807950e993df15cb68aa942
Zulfan <zlfnhmd@gmail.com>
2020-08-29 19:40:45 +0530

    add prerequisites script

---
+++ prerequisites.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Installing the bare necessities. The rest of the stuff will be installed with an interactive cli
+
+apt install curl
+
+apt-get install git
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts
